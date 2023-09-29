4a15ae5376fc076f104612ed1bcf30706b6b30f0
arshannon9 <66278336+arshannon9@users.noreply.github.com>
2023-09-29 09:26:56 -0400

    Implement entry pages and search function

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+import markdown2
+
+from django.http import Http404
+from django.shortcuts import redirect, render
 
 from . import util
 
@@ -8,3 +11,30 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry_name):
+    content = util.get_entry(entry_name)
+    if content:
+        html_content = markdown2.markdown(content)
+        return render(request, "encyclopedia/entry.html", {'entry': html_content})
+    else:
+        raise Http404("Entry does not exist")
+
+def search(request):
+    # Get query from request
+    query = request.GET.get('q')
+
+    # Get list of entries for comparison with query
+    entries = util.list_entries()
+
+    # If query exactly matches an entry name, redirect to entry's page
+    if query in entries:
+        return redirect('entry', entry_name=query)
+
+    # If query not an exact match, search entries for partial matches
+    else:
+        matches = [entry for entry in entries if query in entry]
+
+        # Render search results template with matches
+        return render(request, 'search_results.html', {
+            'matches': matches
+        }) 
